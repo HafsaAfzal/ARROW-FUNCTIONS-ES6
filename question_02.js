d50ac96afb132cbe882a9e7c65c0ac85c42276ef
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-10 12:40:59 +0500

    question-2 code added

---
+++ question_02.js
@@ -0,0 +1,6 @@
+// Write an arrow function that takes a string and returns its length.
+const howLong=(string)=>{
+    return string.length;
+}
+let text="My name is Hafsa.";
+console.log(howLong(text));
