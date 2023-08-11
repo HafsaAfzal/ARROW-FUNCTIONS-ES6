003d5e09b2d64e404bfc09b01d13255b5adb75b5
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 09:41:35 +0500

    question 6 code added

--- question_06.js
+++ question_06.js
@@ -1,2 +1,7 @@
 // Write an arrow function that takes no parameters and returns a random number
 // between 0 and 1.
+const ranNum=()=>{
+    let number=Math.random();
+    return number
+}
+console.log(ranNum());
